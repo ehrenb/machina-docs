85ffb820cdb709ece8b82970ced16156f611e196
ehrenb <branden.a.ehrenreich@gmail.com>
2022-12-31 14:27:58 -0500

    revert docutils to fix bulleted lists, according to https://github.com/readthedocs/sphinx_rtd_theme/issues/1115

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,10 @@ author = 'Branden Ehrenreich'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
